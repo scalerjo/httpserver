955f17d09a9c4334b5773f6f49965e413c7ae830
scalerjo <59584411+scalerjo@users.noreply.github.com>
2020-12-08 13:41:39 -0500

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,1 +1,21 @@
+#include "HttpServerBuilder.h"
+#include "Router.cpp"
 
+int main(int argc, char *argv[])
+{
+    //WARNING, You are responsible for inputting command line arguments and checking their validity!
+
+    //Create our defined router
+    Router MyRouter;
+
+    //Reference ServerStartDetails.h for Argument Params
+    ServerStartDetails SSD(argv);
+
+    //Pass our router and server settings to the server builder
+    ServerBuilder Server(MyRouter, SSD);
+
+    //Start the server!
+    Server.Start();
+    
+    return 0;
+}
